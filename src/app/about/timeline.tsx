0169ab90130d03b02a0f855882ec06909a238b1b
Franz Sinaga <sinagafranz12@gmail.com>
2024-08-15 20:41:28 +0700

    animate: work experience content

--- src/app/about/timeline.tsx
+++ src/app/about/timeline.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import BlurFade from '@/components/magicui/blur-fade'
 import { Tabs } from '@/components/ui/tabs'
 
 export default function Timeline() {
@@ -10,17 +11,21 @@ export default function Timeline() {
       value: 'bri',
       content: (
         <div className={tabClass}>
-          <p className=' text-xl font-bold md:text-4xl'>Bank Rakyat Indonesia, Tbk</p>
-          <p className='text-md mb-2 text-white'>
-            <strong className='ffont-bold'>Frontend Developer</strong> | March 2021 - January 2023
-          </p>
-          <ul className='ml-5 mt-5 list-disc space-y-2 font-sans text-base text-white'>
-            <li>Building NDS (New Delivery System) applications using Quasar Framework (Vue)</li>
-            <li>Implementing Vuex as state management on web applications to facilitate application development</li>
-            <li>Implement Protobuf (Protocol Buffer) as a communication medium between Frontend and Backend to speed up data exchange</li>
-            <li>Implementing PWAs to optimize web applications</li>
-            <li>Collaborate with Teams using the Agile Scrum method to speed up the web application development process</li>
-          </ul>
+          <BlurFade inView delay={0.2}>
+            <p className=' text-xl font-bold md:text-4xl'>Bank Rakyat Indonesia, Tbk</p>
+            <p className='text-md mb-2 text-white'>
+              <strong className='font-bold'>Frontend Developer</strong> | March 2021 - January 2023
+            </p>
+          </BlurFade>
+          <BlurFade inView delay={0.4}>
+            <ul className='ml-5 mt-5 list-disc space-y-2 font-sans text-base text-foreground'>
+              <li>Building NDS (New Delivery System) applications using Quasar Framework (Vue)</li>
+              <li>Implementing Vuex as state management on web applications to facilitate application development</li>
+              <li>Implement Protobuf (Protocol Buffer) as a communication medium between Frontend and Backend to speed up data exchange</li>
+              <li>Implementing PWAs to optimize web applications</li>
+              <li>Collaborate with Teams using the Agile Scrum method to speed up the web application development process</li>
+            </ul>
+          </BlurFade>
         </div>
       )
     },
@@ -29,15 +34,19 @@ export default function Timeline() {
       value: 'lawencon',
       content: (
         <div className={tabClass}>
-          <p className=' text-xl font-bold md:text-4xl'>Lawencon</p>
-          <p className='text-md mb-2 text-white'>
-            <strong className='ffont-bold'>Frontend Developer</strong> | January 2021 - March 2021
-          </p>
-          <ul className='ml-5 mt-5 list-disc space-y-2 font-sans text-base text-white'>
-            <li>Building a LinovHR web application using the Angular framework</li>
-            <li>Implementing Redux as state management on web applications to facilitate application development</li>
-            <li>Working with other developers to implement the RESTFul API in web application development</li>
-          </ul>
+          <BlurFade inView delay={0.2}>
+            <p className=' text-xl font-bold md:text-4xl'>Lawencon</p>
+            <p className='text-md mb-2 text-white'>
+              <strong className='font-bold'>Frontend Developer</strong> | January 2021 - March 2021
+            </p>
+          </BlurFade>
+          <BlurFade inView delay={0.4}>
+            <ul className='ml-5 mt-5 list-disc space-y-2 font-sans text-base text-foreground'>
+              <li>Building a LinovHR web application using the Angular framework</li>
+              <li>Implementing Redux as state management on web applications to facilitate application development</li>
+              <li>Working with other developers to implement the RESTFul API in web application development</li>
+            </ul>
+          </BlurFade>
         </div>
       )
     },
@@ -46,15 +55,19 @@ export default function Timeline() {
       value: 'pariwisata-toba',
       content: (
         <div className={tabClass}>
-          <p className=' text-xl font-bold md:text-4xl'>Komunitas Pariwisata Toba</p>
-          <p className='text-md mb-2 text-white'>
-            <strong className='ffont-bold'>Freelance Mobile Developer</strong> | October 2020 - January 2021
-          </p>
-          <ul className='ml-5 mt-5 list-disc space-y-2 font-sans text-base text-white'>
-            <li>Build a mobile application SIAPPARA-INANGBAO (Sistem Informasi Pemungutan dan Pelaporan Retribusi Pasar) using React Native</li>
-            <li>Designing business processes, databases, and mobile application interface displays</li>
-            <li>Successfully assisted market officials in administering user fees at twelve markets in the Humbang Hasundutan district</li>
-          </ul>
+          <BlurFade inView delay={0.2}>
+            <p className=' text-xl font-bold md:text-4xl'>Komunitas Pariwisata Toba</p>
+            <p className='text-md mb-2 text-white'>
+              <strong className='font-bold'>Freelance Mobile Developer</strong> | October 2020 - January 2021
+            </p>
+          </BlurFade>
+          <BlurFade inView delay={0.4}>
+            <ul className='ml-5 mt-5 list-disc space-y-2 font-sans text-base text-foreground'>
+              <li>Build a mobile application SIAPPARA-INANGBAO (Sistem Informasi Pemungutan dan Pelaporan Retribusi Pasar) using React Native</li>
+              <li>Designing business processes, databases, and mobile application interface displays</li>
+              <li>Successfully assisted market officials in administering user fees at twelve markets in the Humbang Hasundutan district</li>
+            </ul>
+          </BlurFade>
         </div>
       )
     },
@@ -63,17 +76,21 @@ export default function Timeline() {
       value: 'jubelio',
       content: (
         <div className={tabClass}>
-          <p className=' text-xl font-bold md:text-4xl'>Jubelio</p>
-          <p className='text-md mb-2 text-white'>
-            <strong className='font-bold'>Intern Web Developer</strong> | October 2020 - January 2021
-          </p>
-          <ul className='ml-5 mt-5 list-disc space-y-2 font-sans text-base text-white'>
-            <li>
-              Build a Jubelio Finance System using Laravel, a medium for recording customer fines on the use of the Jubelio platform so that it helps companies increase revenue
-            </li>
-            <li>Re-engineering the Analisa Data Penjualan Jubelio system to help customers improve their sales strategy</li>
-            <li>Create SQL Query related to transaction reports, to assist users in managing finances</li>
-          </ul>
+          <BlurFade inView delay={0.2}>
+            <p className=' text-xl font-bold md:text-4xl'>Jubelio</p>
+            <p className='text-md mb-2 text-white'>
+              <strong className='font-bold'>Intern Web Developer</strong> | October 2020 - January 2021
+            </p>
+          </BlurFade>
+          <BlurFade inView delay={0.4}>
+            <ul className='ml-5 mt-5 list-disc space-y-2 font-sans text-base text-foreground'>
+              <li>
+                Build a Jubelio Finance System using Laravel, a medium for recording customer fines on the use of the Jubelio platform so that it helps companies increase revenue
+              </li>
+              <li>Re-engineering the Analisa Data Penjualan Jubelio system to help customers improve their sales strategy</li>
+              <li>Create SQL Query related to transaction reports, to assist users in managing finances</li>
+            </ul>
+          </BlurFade>
         </div>
       )
     }

1932646aa1591bdc00ccc3b26c9325d3a043511d
Franz Sinaga <sinagafranz12@gmail.com>
2024-08-20 21:40:31 +0700

    theme: add content-background colors tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -30,6 +30,7 @@ const config = {
         ring: 'hsl(var(--ring))',
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
+        'content-background': 'hsl(var(--content-background))',
         primary: {
           DEFAULT: 'hsl(var(--primary))',
           foreground: 'hsl(var(--primary-foreground))'

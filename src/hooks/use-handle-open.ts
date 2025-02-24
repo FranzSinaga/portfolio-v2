ee4a526cf83f83106f0f40e849ec76e6876ba1b0
Franz Sinaga <sinagafranz12@gmail.com>
2025-02-24 18:09:05 +0700

    hooks: add use-handle-open

---
+++ src/hooks/use-handle-open.ts
@@ -0,0 +1,37 @@
+import { useEffect, useRef, useState } from 'react'
+
+function useHandleOpen<T extends HTMLElement>(initialState = false) {
+  const [isOpen, setIsOpen] = useState(initialState)
+  const ref = useRef<T | null>(null)
+
+  useEffect(() => {
+    function handleKeyDown(event: KeyboardEvent) {
+      if (event.key === 'Escape') {
+        setIsOpen(false)
+      }
+    }
+
+    function handleClickOutside(event: MouseEvent) {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        setIsOpen(false)
+      }
+    }
+
+    if (isOpen) {
+      document.addEventListener('mousedown', handleClickOutside)
+      document.addEventListener('keydown', handleKeyDown)
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside)
+      document.removeEventListener('keydown', handleKeyDown)
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+      document.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [isOpen])
+
+  return { isOpen, setIsOpen, ref }
+}
+
+export default useHandleOpen

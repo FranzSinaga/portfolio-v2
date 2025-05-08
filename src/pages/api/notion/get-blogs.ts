5f5473aff1f58398e8d9f27a01a8242a10e25e62
Franz Sinaga <sinagafranz12@gmail.com>
2025-05-08 16:11:43 +0700

    Fix: error get blogs

--- src/pages/api/notion/get-blogs.ts
+++ src/pages/api/notion/get-blogs.ts
@@ -1,15 +1,14 @@
 import { parseNotionItem } from '@/lib/notion'
 import { MyNotionItem, NotionDatabaseItem } from '@/types/notion.type'
 import { Client } from '@notionhq/client'
-import { NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   items?: MyNotionItem[]
   error?: string
 }
 
-export default async function handler(res: NextApiResponse<Data>) {
-  // Inisialisasi Notion client
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   const notion = new Client({
     auth: process.env.NOTION_API_KEY!
   })
@@ -17,18 +16,20 @@ export default async function handler(res: NextApiResponse<Data>) {
   const databaseId = process.env.NOTION_DATABASE_ID
 
   try {
-    // Query database
-    const response = await notion.databases.query({
-      database_id: databaseId!,
-      filter: {
-        property: 'Published',
-        checkbox: { equals: true }
-      },
-      sorts: [{ property: 'Created', direction: 'descending' }]
-    })
-    console.log(response)
-    const items = response.results.map(item => parseNotionItem(item as NotionDatabaseItem))
-    res.status(200).json({ items })
+    if (req.method === 'GET') {
+      const response = await notion.databases.query({
+        database_id: databaseId!,
+        filter: {
+          property: 'Published',
+          checkbox: { equals: true }
+        },
+        sorts: [{ property: 'Created', direction: 'descending' }]
+      })
+      const items = response.results.map(item => parseNotionItem(item as NotionDatabaseItem))
+      res.status(200).json({ items })
+    } else {
+      res.status(405).json({ error: 'Method Not Allowed' })
+    }
   } catch (error) {
     console.error('Error querying Notion database:', error)
     res.status(500).json({ error: 'Failed to query Notion database' })
